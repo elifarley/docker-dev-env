d8b4af1003c810c94d47d98c3d3c7d611118d575
Elifarley C <elifarley@users.noreply.github.com>
2016-05-30 17:51:33 -0300

    Alpine's date command doesn't print nanos
    nor anything after "%N", like the timezone

--- install-helper.sh
+++ install-helper.sh
@@ -41,8 +41,8 @@ os_version() { (
 ) }
 
 save_image_info() {
-  date +'%F-%T.%N %Z' >> "$IMAGE_INFO_FILE"
-  printf "BASE_IMAGE: %s\n%s\n(%s)\n" "$BASE_IMAGE" "$(os_version)" "$(uname -rsv)" >> "$IMAGE_INFO_FILE"
+  printf 'Build date: %s %s' "$(date +'%F %T.%N')" "$(date +%Z)" >> "$IMAGE_INFO_FILE"
+  printf 'Base image: %s\n%s\n(%s)\n' "$BASE_IMAGE" "$(os_version)" "$(uname -rsv)" >> "$IMAGE_INFO_FILE"
 }
 
 update_pkg_list() {
