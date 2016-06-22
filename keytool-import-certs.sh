ba248ab90a9528716402349c4cb0aff43f633525
Elifarley C <elifarley@users.noreply.github.com>
2016-06-22 13:11:04 -0300

    New argument "--force"

--- keytool-import-certs.sh
+++ keytool-import-certs.sh
@@ -4,27 +4,43 @@ default_cert_import_dir="${default_cert_import_dir:-/mnt-ssh-config/certs}"
 default_keystore="${default_keystore:-"$JAVA_HOME"/jre/lib/security/cacerts}"
 
 keytool_import_certs() {
-  local cert_import_dir="$1"; test "$1" = '--' && cert_import_dir=''
+  local cert_import_dir=''
+  local force=''; test "$1" = '--force' -o "$1" = '-f' && {
+    force=1; test $# -gt 0 && shift && cert_import_dir="$1"
+  }
+  test "$cert_import_dir" = '--' && cert_import_dir=''
   cert_import_dir="${cert_import_dir:-$default_cert_import_dir}"
   local storepass="${2:-changeit}"
   local keystore="${3:-$default_keystore}"
 
   test -d "$cert_import_dir" && test "$(ls -A "$cert_import_dir" 2>/dev/null)" || {
-    echo "No certificates to import at '$cert_import_dir'"
+    echo "No certificates to import from '$cert_import_dir'"
     return 1
   }
 
   echo "Importing certificates from '$cert_import_dir' into '$keystore'"
+  local v
   local count=0
   for cert in "$cert_import_dir"/*; do
-    echo "Importing '$(basename "$cert")'..."
-    keytool -import -file "$cert" -alias "$(basename "$cert")" -noprompt -storepass "$storepass" \
-    -keystore "$keystore" || return
+    # Find out if alias already exists
+    keytool 2>/dev/null >/dev/null -list -alias "$(basename "$cert")" -noprompt -storepass "$storepass" \
+    -keystore "$keystore" && {
+      test -z "$force" && \
+        v='Skipping existing item' && \
+        printf '%s "%s"...\n' "$v" "$(basename "$cert")" && \
+        continue
+      v='Re-importing'
+      keytool 2>/dev/null >/dev/null -delete -alias "$(basename "$cert")" -noprompt -storepass "$storepass" \
+        -keystore "$keystore" || return
+    } || v='Importing'
+    printf '%s "%s"...\n' "$v" "$(basename "$cert")"
+    keytool 2>&1 -import -file "$cert" -alias "$(basename "$cert")" -noprompt -storepass "$storepass" \
+    -keystore "$keystore" | grep -v JAVA_TOOL_OPTIONS || return
     count=$((count + 1))
   done
 
-  local msg="$count certificate"; test $count -gt 1 && msg="${msg}s"
-  echo "$msg imported."
+  v="$count certificate"; test $count -gt 1 && v="${v}s"
+  echo "$v imported."
 }
 
 keytool_import_certs_interactive() {
