e87f41cfe191f0a27ef7d43fb614e5a1bd48437c
Elifarley C <elifarley@users.noreply.github.com>
2018-11-10 09:36:58 -0200

    Create docker-setup-image.sh

---
+++ wasm-rust/docker-setup-image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+. /app/.cargo/env
+
+rustc --version || exit
+
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh && \
+cargo install cargo-generate
